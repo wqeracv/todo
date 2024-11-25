87a68e499e2e84142f81ffef536a5c96c0b0f097
wqeracv <iseongbeom@iseongbeom-ui-MacBookPro.local>
2024-11-25 18:43:40 +0900

    ✨ Main 함수 구현
    메뉴별 입력값 및 Todo 기초 툴 구

---
+++ todo.py
@@ -0,0 +1,40 @@
+def add_task(task_name): # 1번에 할일을추가하는 함수
+    pass 
+def view_task(): # 2번에 할 일 목록보기를 보여주는 함수
+    pass
+def complete_task(task_number): # 할일 완료 함수
+    pass
+def delete_task(task_number): # 할일 삭제 함수
+    pass
+
+def show_menu():#메뉴를 보여주는 함수
+    print("작업 관리 애플리케이션")
+    print("1. 할 일 추가")
+    print("2. 할 일 목록보기")
+    print("3. 할 일 완료")
+    print("4. 할 일 삭제")
+    print("5. 종료")
+
+def main():
+    while True: 
+        show_menu()
+        choice = input("원하는 작업을 선택해주세요(1~5):")
+
+        if choice == "1":
+            task_name = input("추가할 작업을 입력해주세요") 
+            add_task(task_name)
+        elif choice == "2":
+            view_task()
+        elif choice == "3":
+            task_number = int(input("완료를 원하는 작업의 번호를 입력해주세요"))
+            complete_task(task_number)
+        elif choice == "4":
+            task_number = int(input("삭제를 원하는 작업의 번호를 입력해주세요"))
+            delete_task(task_number)
+
+        elif choice == "5":
+            print("시스템을 종료합니다.")
+            break
+        else :
+            print("잘못 입력하셨습니다. 1부터 5번까지의 기능을 선택하십시오")
+main()
