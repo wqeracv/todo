531a27f8ee9f6bc722426dac8c47cd9cbef07254
wqeracv <iseongbeom@iseongbeom-ui-MacBookPro.local>
2024-11-27 18:49:16 +0900

    ✨ Add_task 기능 구현 및 .gitignore 파일 추가
    tasks.json파일을 포함하지 않도록 .gitignore 파일에 추가

--- todo.py
+++ todo.py
@@ -1,11 +1,32 @@
+import json
+import os # 파이썬을 이용해서 시스템 내부에 접근이 가능하다
+
+
+task_file = 'task.json'
+
+def load_task():
+    if os.path.exists(task_file):
+        with open(task_file, 'r', emcpdomg='utf-8') as file:
+            return json.load(file) #json.load() 함수라고 하지만 메소드는 클래스 안에 구현된 함수다
+    return []
+def save_task(tasks): #add_task르ㄹ 통해 전달받은 해야할일을 파일에 저장하는 기능
+    with open(task_file, 'w', encoding='utf-8') as file: #file => open(task_file, 'w', encoding='utf-8')
+        json.dump(tasks, file, indent=4, ensure_ascii=False)
+
 def add_task(task_name): # 1번에 할일을추가하는 함수
-    pass 
+    tasks = load_task()
+    task = {'name': task_name, "completed":False}
+    tasks.append(task)
+    save_task(tasks)
 def view_task(): # 2번에 할 일 목록보기를 보여주는 함수
     pass
+    
 def complete_task(task_number): # 할일 완료 함수
     pass
+
 def delete_task(task_number): # 할일 삭제 함수
     pass
+
 
 def show_menu():#메뉴를 보여주는 함수
     print("작업 관리 애플리케이션")
